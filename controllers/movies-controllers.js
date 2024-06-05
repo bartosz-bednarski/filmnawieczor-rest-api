4953e09eb5105e79ec4954d3085a5877a6b643ab
Bartosz Bednarski <bambocha1997@gmail.com>
2024-06-05 12:43:20 +0200

    Added route and controller for filtered movies.

--- controllers/movies-controllers.js
+++ controllers/movies-controllers.js
@@ -2,11 +2,31 @@ const db = require("./db");
 
 const getAllMovies = async () => {
   const rows = await db.query(
-    "select movies.id,image_cover,name,description,movie_category as category, movie_rating as rating,production_year,action_place,action_time_start,action_time_end,movie_length from movies inner join movie_category on movie_category.movie_id=movies.id inner join movie_rating on movie_rating.movie_id=movies.id inner join production_year on production_year.movie_id=movies.id inner join action_place on action_place.movie_id=movies.id inner join action_time on action_time.movie_id=movies.id INNER join movie_length on movie_length.movie_id=movies.id"
+    "SELECT m.id AS 'id', m.name AS 'name', m.description AS 'description', m.image_cover AS 'image_cover', GROUP_CONCAT(DISTINCT ap.action_place SEPARATOR ', ') AS 'action_place', MIN(at.action_time_start) AS 'action_time_start', MAX(at.action_time_end) AS 'action_time_end', GROUP_CONCAT(DISTINCT mc.movie_category SEPARATOR ', ') AS 'category', ml.movie_length AS 'movie_length', mr.movie_rating AS 'rating', py.production_year AS 'production_year' FROM movies m LEFT JOIN action_place ap ON m.id = ap.movie_id LEFT JOIN action_time at ON m.id = at.movie_id LEFT JOIN movie_category mc ON m.id = mc.movie_id LEFT JOIN movie_length ml ON m.id = ml.movie_id LEFT JOIN movie_rating mr ON m.id = mr.movie_id LEFT JOIN production_year py ON m.id = py.movie_id GROUP BY m.id, m.name, m.description, m.image_cover, ml.movie_length, mr.movie_rating, py.production_year;"
   );
   return rows;
 };
 
+const getFilteredMovies = async (req) => {
+  const queryFilters = req.data
+    .map((item, index) => {
+      if (req.data.length === 1) {
+        return `${item.queryName}="${item.queryValue}" `;
+      } else if (req.data.length > 0) {
+        if (index !== req.data.length - 1) {
+          return `${item.queryName}="${item.queryValue}" AND`;
+        } else {
+          return `${item.queryName}="${item.queryValue}"`;
+        }
+      }
+    })
+    .join(" ");
+  const queryParams = `SELECT m.id AS 'id', m.name AS 'name', m.description AS 'description', m.image_cover AS 'image_cover', GROUP_CONCAT(DISTINCT ap.action_place SEPARATOR ', ') AS 'action_place', MIN(at.action_time_start) AS 'action_time_start', MAX(at.action_time_end) AS 'action_time_end', GROUP_CONCAT(DISTINCT mc.movie_category SEPARATOR ', ') AS 'category', ml.movie_length AS 'movie_length', mr.movie_rating AS 'rating', py.production_year AS 'production_year' FROM movies m LEFT JOIN action_place ap ON m.id = ap.movie_id LEFT JOIN action_time at ON m.id = at.movie_id LEFT JOIN movie_category mc ON m.id = mc.movie_id LEFT JOIN movie_length ml ON m.id = ml.movie_id LEFT JOIN movie_rating mr ON m.id = mr.movie_id LEFT JOIN production_year py ON m.id = py.movie_id WHERE ${queryFilters} GROUP BY m.id, m.name, m.description, m.image_cover, ml.movie_length, mr.movie_rating, py.production_year;`;
+  const rows = await db.query(queryParams);
+  // console.log(rows);
+  return rows;
+};
 module.exports = {
   getAllMovies,
+  getFilteredMovies,
 };
--- routes/movies-routes.js
+++ routes/movies-routes.js
@@ -14,4 +14,20 @@ router.get("/", async (req, res, next) => {
     next(error);
   }
 });
+router.post("/filter", async (req, res, next) => {
+  try {
+    // console.log(req.body);
+    // res.json({ message: "ok" });
+    const data = await movies.getFilteredMovies(req.body);
+    res.json(data);
+    // const result = await db.query("SELECT * FROM movie_category");
+    // console.log("results", result);
+    // res.json(result);
+    // // res.json(result);
+  } catch (error) {
+    console.error(`Error while getting programming languages `, error.message);
+    next(error);
+  }
+});
+
 module.exports = router;
