52e9eae4f0e59a228577db8d4ff43361e53fc7c9
Bartosz Bednarski <bambocha1997@gmail.com>
2024-06-28 09:39:05 +0200

    Added ready_to_publish condition at all queries

--- controllers/news-controllers.js
+++ controllers/news-controllers.js
@@ -1,13 +1,13 @@
 const db = require("./db");
 const getLast10News = async () => {
   const rows = await db.query(
-    "select id,url,title,type,image_cover,cover_content from news order by id desc limit 10;"
+    "select id,url,title,type,image_cover,cover_content from news WHERE ready_to_publish=1 order by id desc limit 10;"
   );
   return rows;
 };
 const getNewsDetails = async (newsUrl) => {
   const rows = await db.query(
-    `select n.title AS "news_title",n.type,n.id as "news_id",nac.title AS "article_title",nac.image AS "article_image", nac.content AS "article_content", nac.id AS "article_id" FROM news_article_content nac JOIN news n ON nac.news_id=n.id where n.url='${newsUrl}'`
+    `select n.title AS "news_title",n.type,n.id as "news_id",nac.title AS "article_title",nac.image AS "article_image", nac.content AS "article_content", nac.id AS "article_id" FROM news_article_content nac JOIN news n ON nac.news_id=n.id where n.ready_to_publish=1 AND n.url='${newsUrl}'`
   );
 
   const newsTitle = rows[0].news_title;
