58e4a50c5245cca9153b5bbd39ef5c7537804d81
Bartosz Bednarski <bambocha1997@gmail.com>
2025-01-30 12:27:37 +0100

    Fix

--- controllers/news-controllers.js
+++ controllers/news-controllers.js
@@ -9,12 +9,10 @@ const getAllIds = async () =>{
 const rows = await db.query('SELECT id FROM news;');
 return rows;
 };
-
 const getNewsDetails = async (newsUrl) => {
   const rows = await db.query(
     `select n.title AS "news_title",n.type,n.id as "news_id",nac.title AS "article_title",nac.image AS "article_image",nac.url AS "article_url", nac.content AS "article_content", nac.id AS "article_id" FROM news_article_content nac JOIN news n ON nac.news_id=n.id where n.ready_to_publish=1 AND n.url='${newsUrl}'`
   );
-
   const newsTitle = rows[0].news_title;
   const rowsWithoutNewsTitle = rows.map(
     ({
